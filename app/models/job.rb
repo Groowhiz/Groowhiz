d0da67050e29ec020d90ada6504512e7ac092b38
saiteja55 <saitejareddy55@gmail.com>
2015-12-16 19:03:39 +0530

    Testing Branch

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,5 +1,13 @@
 class Job < ActiveRecord::Base
+  include RankedModel
+  include ERB::Util
+
+
   belongs_to :project
   belongs_to :category
   belongs_to :job_reward
+
+
+
+  delegate :display_description, :display_deliver_estimate, to: :decorator
 end
