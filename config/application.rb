fef0d864ce0db543b502bfc3b1a846c200f70dc0
anuragmusic2010 <anuragmusic2010@gmail.com>
2015-12-16 13:03:59 +0530

    New Changes

--- config/application.rb
+++ config/application.rb
@@ -49,6 +49,7 @@ module Catarse
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
 
+    config.i18n.available_locales=[:en,:pt]
 
     # TODO: remove
     config.active_record.whitelist_attributes = false
